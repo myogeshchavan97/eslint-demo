81ff5068f7ea2b5b061794671c82d16107ad4eb1
Yogesh <yogesh@Avinashs-MacBook-Air.local>
2020-05-20 13:24:56 +0530

    changes for react

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const user = 'Harry';
-console.log(user);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = () => {
+  return <h1>This is some JSX</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
